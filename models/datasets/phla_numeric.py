d017e67afe06bd7ac32d9f2e8c9f83270286d10c
Matthew Bakalar <matthew.bakalar@gmail.com>
2023-03-08 10:24:03 -0500

    Added cross fold val. First test.

--- models/datasets/phla_numeric.py
+++ models/datasets/phla_numeric.py
@@ -6,6 +6,8 @@ import torch.utils.data as torch_data
 import torch.nn.functional as F
 
 from torch.utils.data import DataLoader
+from sklearn.model_selection import KFold
+
 
 from .phla_numeric_data import NumericDataset
         
@@ -19,7 +21,10 @@ class DataModule(L.LightningDataModule):
         train_test_split: float,
         batch_size: int,
         predict_mode: bool=False,
-        normalize: bool=False
+        normalize: bool=False,
+        k: int = 1,  # fold number
+        split_seed: int = 12345,  # split needs to be always the same for correct cross validation
+        num_splits: int = 10,
     ):
         super().__init__()
 
@@ -30,6 +35,9 @@ class DataModule(L.LightningDataModule):
         self.batch_size = batch_size
         self.predict_mode = predict_mode
         self.normalize = normalize
+        self.k = k
+        self.split_seed = split_seed
+        self.num_splits = num_splits
 
     def prepare_data(self):
         # Set up peptide dataset
@@ -43,13 +51,16 @@ class DataModule(L.LightningDataModule):
         # Assign train/val datasets for use in dataloaders
         dataset_size = len(self.peptide_dataset)
         indices = list(range(dataset_size))
-        split = int(np.floor(self.train_test_split * dataset_size))
+
+        # K-fold selection
+        # choose fold to train on
+        kf = KFold(n_splits=self.num_splits, shuffle=True, random_state=self.split_seed)
+        all_splits = [k for k in kf.split(indices)]
             
         if self.predict_mode:
             train_indices, val_indices = ([], indices)
         else:
-            np.random.shuffle(indices)
-            train_indices, val_indices = indices[split:], indices[:split]
+            train_indices, val_indices = all_splits[self.k]
 
         self.train_indices = train_indices
         self.val_indices = val_indices
